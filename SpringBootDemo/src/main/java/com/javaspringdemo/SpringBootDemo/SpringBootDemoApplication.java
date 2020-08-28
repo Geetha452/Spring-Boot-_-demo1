a45ca8cb6682eba8f9f507a1627dcc6f93556e8c
geeth <geeth@10.153.150.72>
2020-08-27 19:44:39 -0500

    using JPA data accessing, and jasper report generation

--- SpringBootDemo/src/main/java/com/javaspringdemo/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/javaspringdemo/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,12 +1,50 @@
 package com.javaspringdemo.SpringBootDemo;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaspringdemo.SpringBootDemo.Entity.Employee;
+import com.javaspringdemo.SpringBootDemo.Service.ReportService;
+import com.javaspringdemo.SpringBootDemo.repository.EmployeeRepository;
+import com.javaspringdemo.SpringBootDemo.repository.UserRepository;
+
+import net.sf.jasperreports.engine.JRException;
 
 @SpringBootApplication
-
+@RestController
 public class SpringBootDemoApplication {
 
+	
+	@Autowired
+	private EmployeeRepository repository;
+	@Autowired
+	private ReportService service;
+	
+	@GetMapping("/getuserdetails")
+	public List<Employee> getUserDetails(){
+		
+		for (Employee record : repository.findAll()) {
+		  if(repository.findById(1) != null)
+			  System.out.println("true" + repository.findById(1) );
+		}
+		
+		return (List<Employee>) repository.findAll();
+	}
+	
+	@GetMapping("/report/{format}")
+	//mentioning format of the report through request url
+	public String generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
+		return service.exportReport(format);
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
